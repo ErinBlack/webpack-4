f1cb912dfca124b0e27b109bb5853e0d153cb3d3
Erin Black <erin.black@fieldnation.com>
2018-05-17 08:01:25 -0500

    installed html and css reader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+//
+// PATH OF THIS FILE: ./src/index.js
+//
+import style from "./main.css";
+
+
 console.log(`I'm a silly entry point`);
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
